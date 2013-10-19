94362eac36945067299a68049585edbd95458fff
Nik <nik@khilnani.org>
2013-10-18 20:56:25 -0400

    Update Session.php

--- app/Session.php
+++ app/Session.php
@@ -1,56 +1,44 @@
 <?php
 
-class Session
-{
+class Session {
     
-    static public function logoff()
-    {
+    static public function logoff() {
         global $_SESSION, $_COOKIE;
         
         $_SESSION = array();
-    	if (isset($_COOKIE[session_name()])) 
-        {
-       	    setcookie(session_name(), '', time()-42000, '/');
-    	}
-    	session_destroy();
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '', time() - 42000, '/');
+        }
+        session_destroy();
     }
     
-    static public function login(&$tmplData)
-    {
+    static public function login(&$tmplData) {
         global $ini, $_POST, $_SESSION;
         
-        if(!($_POST['username'] == $ini['userName'] && $_POST['password'] == $ini['password'])) 
-        {
-    		Render::error($tmplData, "The login details were incorrect");
+        if (!($_POST['username'] == $ini['userName'] && $_POST['password'] == $ini['password'])) {
+            Render::error($tmplData, "The login details were incorrect");
             return false;
-    	} 
-        else 
-        {
-    		$_SESSION['user'] = $ini['userName'];
-    	}
+        } else {
+            $_SESSION['user'] = $ini['userName'];
+        }
         
         return true;
     }
     
-    static public function isLoggedIn()
-    {
+    static public function isLoggedIn() {
         global $ini, $_SESSION;
-            
-        if($ini['loginRequired'] == 1) 
-        { 
-            if($_SESSION['user'] == $ini['userName']) 
-            {
+        
+        if ($ini['loginRequired'] == 1) {
+            if ($_SESSION['user'] == $ini['userName']) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
         }
         
         return true;
     }
-
+    
 }
 
 ?>
